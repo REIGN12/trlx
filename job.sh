dc76601195187dc58255cef161d1262c2dd1d063
reign12 <3180105232@zju.edu.cn>
2023-05-11 18:32:52 +0000

    Job: add gptj-baseline,ppoepoch2-steps20000,ppoepoch1-steps40000

---
+++ job.sh
@@ -0,0 +1,9 @@
+# 2023.5.12
+git checkout hh_exp/gptj-baseline
+bash exp.sh
+
+git checkout hh_exp/gptj-ppoepoch2-steps20000
+bash exp.sh
+
+git checkout hh_exp/gptj-ppoepoch1-steps40000
+bash exp.sh
